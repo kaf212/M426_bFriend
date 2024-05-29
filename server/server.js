7aeb81b0ad827444aa6e9f82008c48aff8622892
kaf212 <kaf221122@gmail.com>
2024-05-29 11:00:16 +0200

    fixed mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const server = express();
 const {connect} = require("mongoose");
+const Users = require('./models/user-model.js');
+
 const cors = require("cors")
 
 server.use(cors({
@@ -9,12 +11,28 @@ server.use(cors({
 
 server.use(express.json())
 
-connect("mongodb://27017", {
-    useNewUrlParser: true
+connect("mongodb://127.0.0.1:27017/bFriend", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
-    .then(() => console.log('MongoDB connected'))
+    .then(() => {
+        console.log('MongoDB connected');
+        return insertTestdata(); // Ensure test data is inserted only after successful connection
+    })
     .catch(err => console.log(err));
 
+async function insertTestdata() {
+    // insert testdata into DB
+    await Users.insertMany([
+        {
+            username: "Test",
+            email: "test@email.com",
+            password: "asdfasdf"
+        }
+    ]);
+}
+
+insertTestdata()
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
