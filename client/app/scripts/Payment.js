b7994dea9c6b6a5bdf7b811225ec9a2d94db4c45
BarmettlerL-bzz <BarmettlerL@bzz.ch>
2024-05-29 10:11:35 +0200

    layout

---
+++ client/app/scripts/Payment.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let submitButton = document.getElementById('submit')
+
+    submitButton.addEventListener('click', ()=>{
+        //check database if data is correct
+
+        //show confirmation, if accepts continue
+
+        //send data to api so the databse updates to an active user with the chosen subscribtion, and backend takes money on successful update
+
+        //return a answer if successful, else show problem
+        alert('button pressed')
+    })
+})
